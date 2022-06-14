38ea0fc8121ef73a6e21609f994562473fb3197f
decook001 <shoaibtasrif326@gmail.com>
2022-06-14 19:55:43 +0600

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+});
+
+var typed = new Typed("#typing", {
+    strings: [ "software engineering undergraduate", "competitive programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    smartBackspace: true,
+    loop: true
+});
+    
+var typed2 = new Typed("#typing-2", {
+    strings: [ "software engineering undergraduate", "competitive programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
